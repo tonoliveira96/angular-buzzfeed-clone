12db5893fe99bc3bc07ca86359015ead1e59b73e
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-11-02 11:59:58 -0300

    feat: Criando propriedades

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './quiz.component.css'
 })
 export class QuizComponent {
+  title: string = "";
 
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answersSelected: string = "";
+
+  questionIndex = 0;
+  questionMaxIndex = 0;
+
+  finished: boolean = false
 }
